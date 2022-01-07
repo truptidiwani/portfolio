89ed5e189e77808a2e0e2ef151c65dad3138fb95
truptidiwani <diwanitrupti@gmail.com>
2022-01-07 19:00:36 +0530

    Mode working

--- JS/action.js
+++ JS/action.js
@@ -1,4 +1,4 @@
-const checkbox =document.getElementById('checkbox');
-checkbox.addEventListener('change',()=>{
-    document.body.classList.toggle('dark');
-});
+// const checkbox =document.getElementById('checkbox');
+// checkbox.addEventListener('change', () => {
+//     document.body.classList.toggle('dark');
+// });
